06fcf04fe178be180be7f5a4616e23bd046de061
JustinRice <jsrice7391@gmail.com>
2020-04-07 21:33:39 -0400

    fix(auth_issue): This gets past the auth issue for now, this would not require the token, but I feel like the strategey is beyond thsi point.

--- client/src/SearchProvider.js
+++ client/src/SearchProvider.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react'
 import API from "./utils/API";
 import AuthService from "./components/AuthService";
-import {withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
 // Set Up The Initial Context
 const SearchContext = React.createContext()
@@ -11,81 +11,81 @@ export const SearchConsumer = SearchContext.Consumer
 // Create the provider using a traditional React.Component class
 
 class SearchProvider extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.Auth = new AuthService();
   }
 
-    state = {
-        userName: "",
-        search: "",
-        categories: ["Dining", "Essentials", "Take-Out", "Delivery", "Retail", "Services"],
-        listings: [],
-        error: true,
-        button: "Submit",
-        className: "btn btn-success search-button",
-        handleInputChange: (event) => {
-        this.setState({ ...this.state, search: event.target.value });
-        },
+  state = {
+    userName: "",
+    search: "",
+    categories: ["Dining", "Essentials", "Take-Out", "Delivery", "Retail", "Services"],
+    listings: [],
+    error: true,
+    button: "Submit",
+    className: "btn btn-success search-button",
+    handleInputChange: (event) => {
+      this.setState({ ...this.state, search: event.target.value });
+    },
 
-        handleFormSubmit: (event) => {
-        event.preventDefault();
-        // history.push("/students");
-        console.log("SEARCH", this.state.search)
-        API.getListings(this.state.search)
-          .then(res => {
-            if (res.data.status === "error") {
-              throw new Error(res.data.message);
-            }
-            console.log("RESDATA", res.data)
-            this.setState({ ...this.state, listings: res.data, error: false, search: "" }, () =>{
-              this.props.history.push("/listings")
-            });
-          })
+    handleFormSubmit: (event) => {
+      event.preventDefault();
+      // history.push("/students");
+      console.log("SEARCH", this.state.search)
+      API.getListings(this.state.search)
+        .then(res => {
+          if (res.data.status === "error") {
+            throw new Error(res.data.message);
+          }
+          console.log("RESDATA", res.data)
+          this.setState({ ...this.state, listings: res.data, error: false, search: "" }, () => {
+            this.props.history.push("/listings")
+          });
+        })
 
-            .catch(err => this.setState({ ...this.state, error: err.message }));
-        }, 
-        loadListings: () => {
-          API.getListings()
-            .then(res =>{
-              console.log("RES22222", res)
-              this.setState({ ...this.state, listings: res.data })
-            })
-            .catch(err => console.log(err));
-        },
-        handleLogin: event => {
-          event.preventDefault();
-      
-          this.Auth.login(this.state.email, this.state.password)
-            .then(res => {
-              // once user is logged in
-              // take them to their profile page
-              this.props.history.replace(`/`);
-            })
-            .then(() => window.location.reload(false))
-            .catch(err => {
-              console.log(err.response.data.message);
-            });
-        }
-      };
+        .catch(err => this.setState({ ...this.state, error: err.message }));
+    },
+    loadListings: () => {
+      API.getListings()
+        .then(res => {
+          console.log("RES22222", res)
+          this.setState({ ...this.state, listings: res.data })
+        })
+        .catch(err => console.log(err));
+    },
+    handleLogin: event => {
+      event.preventDefault();
 
-      componentDidMount() {
-        API.getListings(this.state.search)
-          .then(res => {
-            if (res.data.status === "error") {
-              throw new Error(res.data.message);
-            }
-            console.log("RESDATA", res.data)
-            this.setState({ ...this.state, listings: res.data, error: false });      
-            })
-          .catch(err => this.setState({ ...this.state, error: err.message }));
-      };
+      this.Auth.login(this.state.email, this.state.password)
+        .then(res => {
+          // once user is logged in
+          // take them to their profile page
+          this.props.history.replace(`/`);
+        })
+        .then(() => window.location.reload(false))
+        .catch(err => {
+          console.log(err.response.data.message);
+        });
+    }
+  };
 
-  render () {
+  // componentDidMount() {
+  //   API.getListings(this.state.search)
+  //     .then(res => {
+  //       if (res.data.status === "error") {
+  //         throw new Error(res.data.message);
+  //       }
+  //       console.log("RESDATA", res.data)
+  //       this.setState({ ...this.state, listings: res.data, error: false });
+  //     })
+  //     .catch(err => this.setState({ ...this.state, error: err.message }));
+  // };
+
+  render() {
     return (
       // value prop is where we define what values 
       // that are accessible to consumer components
-       <SearchContext.Provider value={{state: this.state}}>
+      <SearchContext.Provider value={{ state: this.state }}>
         {this.props.children}
       </SearchContext.Provider>
     )
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -16,105 +16,105 @@ class Nav extends React.Component {
   }
 
 
-render() {
-  const Auth = new AuthService();
-  function showNavigation() {
-    if (Auth.loggedIn()) {
-      
-      loggedInUser = Auth.getProfile();
-  
-      return (
-    <div className="navbar-buttons">
-    <ul className="navbar-nav navbar-center">
-    <Link to="/">
-      <li className="nav-item">
-        <div className="nav-link">Help For Businesses</div>
-      </li>
-    </Link>
-    <Link push to="/signup">
-    <li className="nav-item">
-      <div className="nav-link">Add your listing</div>
-    </li>
-    </Link>
-    <Link to="/">
-      <li className="nav-item">
-        <div className="nav-link">Contact</div>
-      </li>
-    </Link>
-    <Link to={"/account/" + loggedInUser.id}>
-      <li className="nav-item">
-        <div className="nav-link">My account</div>
-      </li>
-    </Link>
-    {/* <Link push to="/login">
+  render() {
+    const Auth = new AuthService();
+    function showNavigation() {
+      if (Auth.loggedIn()) {
+
+        loggedInUser = Auth.getProfile();
+
+        return (
+          <div className="navbar-buttons">
+            <ul className="navbar-nav navbar-center">
+              <Link to="/">
+                <li className="nav-item">
+                  <div className="nav-link">Help For Businesses</div>
+                </li>
+              </Link>
+              <Link push to="/signup">
+                <li className="nav-item">
+                  <div className="nav-link">Add your listing</div>
+                </li>
+              </Link>
+              <Link to="/">
+                <li className="nav-item">
+                  <div className="nav-link">Contact</div>
+                </li>
+              </Link>
+              <Link to={"/account/" + loggedInUser.id}>
+                <li className="nav-item">
+                  <div className="nav-link">My account</div>
+                </li>
+              </Link>
+              {/* <Link push to="/login">
       <li className="nav-item">
         <div className="nav-link">Login</div>
       </li>
     </Link> */}
-    <Link push to="/">
-      <li className="nav-item">
-        <div className="nav-link" href="/" onClick={() => Auth.logout()}>Logout</div>
-      </li>
-    </Link>
-    </ul>
-  </div>
+              <Link push to="/">
+                <li className="nav-item">
+                  <div className="nav-link" href="/" onClick={() => Auth.logout()}>Logout</div>
+                </li>
+              </Link>
+            </ul>
+          </div>
 
-    );
+        );
 
-  } else {
-    return (
-        <div className="navbar-buttons">
-        <ul className="navbar-nav navbar-center">
-        <Link to="/">
-          <li className="nav-item">
-            <div className="nav-link">Help For Businesses</div>
-          </li>
-        </Link>
-        <Link push to="/signup">
-        <li className="nav-item">
-          <div className="nav-link">Add your listing</div>
-        </li>
-        </Link>
-        <Link to="/">
-        <li className="nav-item">
-          <div className="nav-link">Contact</div>
-        </li>
-        </Link>
-        <Link push to="/login">
-        <li className="nav-item">
-          <div className="nav-link">Login</div>
-        </li>
-        </Link>
-        {/* <Link push to="/">
+      } else {
+        return (
+          <div className="navbar-buttons">
+            <ul className="navbar-nav navbar-center">
+              <Link to="/">
+                <li className="nav-item">
+                  <div className="nav-link">Help For Businesses</div>
+                </li>
+              </Link>
+              <Link push to="/signup">
+                <li className="nav-item">
+                  <div className="nav-link">Add your listing</div>
+                </li>
+              </Link>
+              <Link to="/">
+                <li className="nav-item">
+                  <div className="nav-link">Contact</div>
+                </li>
+              </Link>
+              <Link push to="/login">
+                <li className="nav-item">
+                  <div className="nav-link">Login</div>
+                </li>
+              </Link>
+              {/* <Link push to="/">
       <li className="nav-item">
         <div className="nav-link" href="/" onClick={() => Auth.logout()}>Logout</div>
       </li>
     </Link> */}
-        </ul>
+            </ul>
+          </div>
+        )
+      }
+
+    }
+
+    return (
+      <div className="jumbotron jumbotron-fluid header">
+        <div className="container">
+          <Link to="/">
+            <img className="hamilton-seal" alt="Hamilton Seal" href="/" src={HamiltonSeal} />
+            <img className="wenham-seal" alt="Wenham Seal" href="/" src={WenhamSeal} />
+          </Link>
+
+          <h1 className="title">Virtual Hamilton-Wenham</h1>
+          <p className="subhead">Serving Community – Providing Peace-of-Mind</p>
+        </div>
+        <nav className="navbar navbar-expand-lg navbar-expand-md navbar-expand-sm bg-dark">
+          {showNavigation()}
+        </nav >
       </div>
     )
-    }
- 
+
   }
-
-return (
-  <div className="jumbotron jumbotron-fluid header">
-  <div className="container">
-  <Link to="/">
-  <img className="hamilton-seal" alt="Hamilton Seal" href="/" src={HamiltonSeal} />
-  <img className="wenham-seal" alt="Wenham Seal" href="/" src={WenhamSeal} />
-  </Link>
-
-    <h1 className="title">Virtual Hamilton-Wenham</h1>
-    <p className="subhead">Serving Community – Providing Peace-of-Mind</p>
-  </div>
-  <nav className="navbar navbar-expand-lg navbar-expand-md navbar-expand-sm bg-dark">
-    {showNavigation()}
-  </nav >
-  </div>
-  )
-
-}
 }
 
 export default Nav;
