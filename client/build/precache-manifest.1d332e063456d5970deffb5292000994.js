60e23899590f2c9eec6e5b2ec2306f95985c0fe6
David Hauck <dhauck@mac.com>
2020-04-15 18:02:17 -0400

    added mailto to contact link

---
+++ client/build/precache-manifest.1d332e063456d5970deffb5292000994.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe4c98c882be5d36223022beb9ec8863",
+    "url": "/static/media/hamilton-seal.fe4c98c8.png"
+  },
+  {
+    "revision": "45f70c914851b6364d45",
+    "url": "/static/css/main.53b74b54.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "26b65b3c69220aba6edc015a20be9519",
+    "url": "/static/media/instagram-logo.26b65b3c.png"
+  },
+  {
+    "revision": "4b087a56c120c26a5648",
+    "url": "/static/js/2.316cbc52.chunk.js"
+  },
+  {
+    "revision": "55be3184a2c1ac07e0077424e34d0e1f",
+    "url": "/static/media/hamilton-background3.55be3184.jpg"
+  },
+  {
+    "revision": "45f70c914851b6364d45",
+    "url": "/static/js/main.ade5b381.chunk.js"
+  },
+  {
+    "revision": "f8dbeac6afc69f81029820328975386d",
+    "url": "/static/media/wenham-seal.f8dbeac6.png"
+  },
+  {
+    "revision": "21d11b570aaab8203cde01c4ea3c46d9",
+    "url": "/static/media/facebook-logo.21d11b57.png"
+  },
+  {
+    "revision": "49bb3e336fb2df580da23f67f8821ea3",
+    "url": "/static/media/twitter-logo.49bb3e33.png"
+  },
+  {
+    "revision": "4b087a56c120c26a5648",
+    "url": "/static/css/2.116fe0dd.chunk.css"
+  },
+  {
+    "revision": "417c25a87002c751bb1200810768ea58",
+    "url": "/index.html"
+  }
+];
--- client/src/components/Decorators/index.js
+++ client/src/components/Decorators/index.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import HamiltonSeal from "../../images/hamilton-seal.png"
 import WenhamSeal from "../../images/wenham-seal.png"
 import { useAuth0 } from "../../react-auth0-spa";
+import "./navbar.css";
 
 export const Links = props => {
   const { isAuthenticated, loginWithRedirect, logout, user} = useAuth0();
@@ -10,21 +11,14 @@ export const Links = props => {
     return (
       <div className="navbar-buttons">
         <ul className="navbar-nav navbar-center">
-          <Link to="/">
-            <li className="nav-item">
-              <div className="nav-link">Help For Businesses</div>
-            </li>
-          </Link>
           <Link push to="/signup">
             <li className="nav-item">
               <div className="nav-link">Add your listing</div>
             </li>
           </Link>
-          <Link to="/">
-            <li className="nav-item">
-              <div className="nav-link">Contact</div>
-            </li>
-          </Link>
+          <li className="nav-item">
+            <div className="nav-link" href="mailto:admin@kinshipfarm.org?subject=Inquiry">Contact</div>
+          </li>
           <Link to={"/account"}>
             <li className="nav-item">
               <div className="nav-link">My account</div>
@@ -52,11 +46,9 @@ export const Links = props => {
               <div className="nav-link">Add your listing</div>
             </li>
           </Link>
-          <Link to="/">
-            <li className="nav-item">
-              <div className="nav-link">Contact</div>
-            </li>
-          </Link>
+          <li className="nav-item">
+            <a className="nav-link" href="mailto:admin@kinshipfarm.org?subject=Inquiry">Contact</a>
+          </li>
           <Link>
           <li className="nav-item">
             <div className="nav-link" onClick={() => loginWithRedirect({})}>Login</div>
