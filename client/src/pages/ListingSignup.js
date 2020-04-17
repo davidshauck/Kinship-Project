d15d68d76fc6bfd0e492fc01c2e3f16b2c798e17
JustinRice <jsrice7391@gmail.com>
2020-04-17 00:35:09 -0400

    feat(formik): Formik is implemented just have to do the error handling

--- client/src/pages/ListingSignup.js
+++ client/src/pages/ListingSignup.js
@@ -5,263 +5,200 @@ import API from "../utils/API";
 import { FormBtn } from "../components/Form";
 import { useAuth0 } from "../react-auth0-spa";
 import { Redirect } from "react-router-dom"
-const OPTIONS = ["Dining", "Essentials", "Take-Out", "Delivery", "Retail", "Services"]
+import { Formik } from "formik";
 
-
-const ListingSignup = props => {
-
-  const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
-  const [listings, setListings] = useState({
-    checkboxes: OPTIONS.reduce(
-      (options, option) => ({
-        ...options,
-        [option]: false
-      }),
-      {}
-    ),
-    options_hooks: OPTIONS,
-    email: "",
-    password: "",
-    name: "",
-    address1: "",
+const ListingForm = (props) => {
+  const form = {
+    user: props.user,
+    email: props.email, name: "", address1: "",
     address2: "",
     city: "",
-    usState: "",
+    us_state: "",
     zip: "",
+    image: "",
     telephone: "",
-    categories: ["On"],
-    photo: "",
     website: "",
     description: "",
-    facebook: "",
+    categories: "",
     twitter: "",
+    facebook: "",
     instagram: "",
-    disabledStatus: true
-  });
+    options: ["Dining", "Essentials", "Take-Out", "Delivery", "Retail", "Services"],
+    chosen: []
+  }
 
-  const handleInputChange = event => {
-    const { name, value } = event.target;
-    setListings({
-      [name]: value
-    });
-  };
-
-  const handleCheckboxChange = changeEvent => {
-    const { name } = changeEvent.target;
-    setListings(prevState => ({
-      checkboxes: {
-        ...prevState.checkboxes,
-        [name]: !prevState.checkboxes[name]
-      }
-    }));
-  };
-
-  const handleLogin = event => {
-    event.preventDefault();
-    this.Auth.login(listings.email, listings.password)
-      .then(res => {
-        // once user is logged in
-        // take them to their profile page
-        this.props.history.replace(`/listing`);
-      })
-      .then(() => window.location.reload(false))
-      .catch(err => {
-        console.log(err.response.data.message);
-      });
-  };
-
-  const handleFormSubmit = formSubmitEvent => {
-    console.log(listings)
-    formSubmitEvent.preventDefault();
-
-
-    Object.keys(listings.checkboxes)
-      .filter(checkbox => listings.checkboxes[checkbox])
-      .forEach(checkbox => {
-        listings.categories.push(checkbox);
-      });
-    API.saveListing({
-      email: user.email,
-      name: listings.name,
-      address1: listings.address1,
-      address2: listings.address2,
-      city: listings.city,
-      state: listings.usState,
-      zip: listings.zip,
-      image: listings.image,
-      telephone: listings.telephone,
-      website: listings.website,
-      description: listings.description,
-      categories: listings.categories,
-      twitter: listings.twitter,
-      facebook: listings.facebook,
-      instagram: listings.instagram
-    }).then(res => {
-      if (res.data._id) {
-        let disabledStatus = false;
-        setListings({ ...listings, disabledStatus: disabledStatus })
-      }
-    })
-
-  };
+  const toggleCheckbox = label => {
+    if (this.selectedCheckboxes.has(label)) {
+      this.selectedCheckboxes.delete(label);
+    } else {
+      this.selectedCheckboxes.add(label);
+    }
+  }
 
   const createCheckbox = option => (
     <Checkbox
       label={option}
-      isSelected={listings.checkboxes[option]}
-      onCheckboxChange={handleCheckboxChange}
+      isSelected={form.chosen[option]}
+      onCheckboxChange={toggleCheckbox}
       key={option}
     />
   );
 
-  //Obj.keys, then loop over that and push the 'true' to an array
+  return (
+    <div>
+      <h4 style={{ color: "black" }}>Create your business profile</h4>
+      <Formik
+        initialValues={form}
+        validate={values => {
+          const errors = {};
+          if (/^\d+$/.test(values.email) === false) {
+            errors.zip = "That is not a valid Zip code"
+          }
+          return errors
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          Object.keys(values.options)
+            .filter(checkbox => values.checkboxes[checkbox])
+            .forEach(checkbox => {
+              values.categories.push(checkbox);
+            });
+          API.saveListing({
+          }).then(res => {
+            alert("Good to go!")
+          })
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting
+        }) => (
+            <form onSubmit={handleSubmit}>
+              <Input
+                name="name"
+                type="text"
+                placeholder="Business Name (required)"
+                value={values.name}
+                className="col-11 signup-boxes"
+                onChange={handleChange}
+              />
+              <Input
+                name="address1"
+                type="text"
+                value={values.address1}
+                className="col-11 signup-boxes"
+                placeholder="Address (required)"
+                onChange={handleChange}
+              />
+              <Input
+                name="address2"
+                type="text"
+                value={values.address2}
+                className="col-11 signup-boxes"
+                placeholder="Address"
+                onChange={handleChange}
+              />
+              <Input
+                name="city"
+                type="text"
+                value={values.city}
+                className="col-5 signup-boxes"
+                placeholder="City"
+                onChange={handleChange}
+              />
+              <Input
+                name="us_state"
+                type="text"
+                value={values.us_state}
+                className="col-2 signup-boxes"
+                placeholder="State"
+                onChange={handleChange}
+              />
+              <Input
+                name="zip"
+                type="text"
+                value={values.zip}
+                className="col-4 signup-boxes"
+                placeholder="Zip*"
+                onChange={handleChange}
+              />
+              <p>{errors.zip && touched.zip && errors.zip}</p>
+              <Input
+                name="telephone"
+                type="text"
+                value={values.telephone}
+                className="col-11 signup-boxes"
+                placeholder="Telephone*"
+                onChange={handleChange}
+              />
+              <Input
+                name="image"
+                type="text"
+                className="col-11 signup-boxes"
+                value={values.user ? values.user.user.picture : ""}
+                placeholder="Photo (paste in url)"
+                onChange={handleChange}
+              />
+              <Input
+                name="website"
+                type="text"
+                value={values.website}
+                className="col-11 signup-boxes"
+                placeholder="Website"
+                onChange={handleChange}
+              />
+              <Input
+                name="twitter"
+                type="text"
+                value={values.twitter}
+                className="col-4 signup-boxes"
+                placeholder="Twitter (full URL)"
+                onChange={handleChange}
+              />
+              <Input
+                name="facebook"
+                type="text"
+                value={values.facebook}
+                className="col-3 signup-boxes"
+                placeholder="Facebook (full URL)"
+                onChange={handleChange}
+              />
+              <Input
+                name="instagram"
+                type="text"
+                value={values.instagram}
+                className="col-4 signup-boxes"
+                placeholder="Instagram (full URL)"
+                onChange={handleChange}
+              />
+              <h4 style={{ color: "black" }}>Categories (check all that apply)</h4>
+              {/* {values.options.map(listing => <p>{listing}</p>)} */}
+              {createCheckbox(values.options)}
+              <div className="col-12">
+                <h4 style={{ color: "black" }}>Additional details</h4>
+                <textarea className="form-control" rows="10" name="description" onChange={handleChange} placeholder="Message to your customers, store hours, etc." />
+                <button type="submit" className="btn btn-secondary save-button">
+                  Save
+                </button>
+              </div>
+            </form>
+          )}
+      </Formik>
+    </div>
+  );
+}
 
-  // const createCheckboxes = () => OPTIONS.map(listings.checkboxes);
+const ListingSignup = props => {
+  const { isAuthenticated, user } = useAuth0();
+
   if (!isAuthenticated) {
     return <Redirect to="login" />
-
   } else {
-    return (
-      <div className="list-overflow-container register-box">
-        <h4 style={{ color: "black" }}>Create your business profile</h4>
-        <form onSubmit={handleFormSubmit}>
-          <Input
-            name="name"
-            type="text"
-            placeholder="Business Name (required)"
-            className="col-11 signup-boxes"
-            onChange={e => handleInputChange(e)}
-          />
-          <Input
-            name="address1"
-            type="text"
-            className="col-11 signup-boxes"
-            placeholder="Address (required)"
-            onChange={e => handleInputChange(e)}
-          />
-          <Input
-            name="address2"
-            type="text"
-            className="col-11 signup-boxes"
-            placeholder="Address"
-            onChange={e => handleInputChange(e)}
-          />
-          <Input
-            name="city"
-            type="text"
-            className="col-5 signup-boxes"
-            placeholder="City"
-            onChange={e => handleInputChange(e)}
-          />
-          <Input
-            name="usState"
-            type="text"
-            className="col-2 signup-boxes"
-            placeholder="State"
-            onChange={e => handleInputChange(e)}
-          />
-          <Input
-            name="zip"
-            type="text"
-            className="col-4 signup-boxes"
-            placeholder="Zip*"
-            onChange={e => handleInputChange(e)}
-          />
-          <Input
-            name="telephone"
-            type="text"
-            className="col-11 signup-boxes"
-            placeholder="Telephone*"
-            onChange={e => handleInputChange(e)}
-          />
-          <Input
-            name="image"
-            type="text"
-            className="col-11 signup-boxes"
-            value={user.picture ? user.picture : ""}
-            placeholder="Photo (paste in url)"
-            onChange={e => handleInputChange(e)}
-          />
-          <Input
-            name="website"
-            type="text"
-            className="col-11 signup-boxes"
-            placeholder="Website"
-            onChange={e => handleInputChange(e)}
-          />
-          <Input
-            name="twitter"
-            type="text"
-            className="col-4 signup-boxes"
-            placeholder="Twitter (full URL)"
-            onChange={e => handleInputChange(e)}
-          />
-          <Input
-            name="facebook"
-            type="text"
-            className="col-3 signup-boxes"
-            placeholder="Facebook (full URL)"
-            onChange={e => handleInputChange(e)}
-          />
-          <Input
-            name="instagram"
-            type="text"
-            className="col-4 signup-boxes"
-            placeholder="Instagram (full URL)"
-            onChange={e => handleInputChange(e)}
-          />
-
-
-          <h4 style={{ color: "black" }}>Categories (check all that apply)</h4>
-          {listings.options_hooks.map(listing => <p>{listing}</p>)}
-
-          <div className="col-12">
-            <h4 style={{ color: "black" }}>Additional details</h4>
-
-            <textarea className="form-control" rows="10" name="description" onChange={e => handleInputChange(e)} placeholder="Message to your customers, store hours, etc." />
-            <form className="div-test">
-
-              {/* <Link push to="/login"> */}
-              <button type="submit" className="btn btn-secondary save-button">
-                Save
-                </button>
-              {/* <button type="submit" href="/login" className="btn btn-success login-button" disabled={listings.disabledStatus} onClick={() => props.history.push("/login")} >
-                    Login
-                </button> */}
-              {!listings.disabledStatus ? (
-                <div>
-                  <input
-                    name="email"
-                    type="email"
-                    id="email"
-                    placeholder="Email address"
-                    className={"form-control login-signup-email-field"}
-
-                    onChange={handleInputChange}
-                  />
-                  {/* </div>
-              <div className="form-group"> */}
-
-                  <input
-                    name="password"
-                    type="password"
-                    id="pwd"
-                    placeholder="Password"
-                    className={"form-control login-signup-password-field"}
-                    onChange={handleInputChange}
-                  />
-                  <FormBtn
-                    button={"Submit"}
-                    onClick={handleLogin}
-                    className={"btn btn-success login-submit-button"}
-                  />
-                </div>
-              ) : (<div></div>)}
-            </form>
-          </div>
-        </form>
-      </div>
-    );
+    return <ListingForm {...user} />
   }
 }
 
