52fa4cc10e36d6574d4365a7a226ca6242108230
JustinRice <jsrice7391@gmail.com>
2020-04-21 23:22:39 -0400

    feat(checkboxes): Checkboxes work

--- client/src/pages/ListingSignup.js
+++ client/src/pages/ListingSignup.js
@@ -1,40 +1,50 @@
 import React, { useState, useEffect } from "react";
-import Checkbox from "../components/Checkbox";
-import { Input } from "../components/Form"
 import API from "../utils/API";
 import { useAuth0 } from "../react-auth0-spa";
 import { Redirect } from "react-router-dom"
-import { Formik, Form } from "formik";
+import { Formik, Form, Field } from "formik";
+import * as Yup from 'yup';
+
+
+const toggleCheckbox = label => {
+  if (this.selectedCheckboxes.has(label)) {
+    this.selectedCheckboxes.delete(label);
+  } else {
+    this.selectedCheckboxes.add(label);
+  }
+}
+
+const ListingSchema = Yup.object().shape({
+  zip_code: Yup.string().matches(/(^\d{5}$)|(^\d{5}-\d{4}$)/, { message: "That is not a valid zip code" })
+    .required('Required'),
+  name: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  address1: Yup.string().matches(/^\s*\S+(?:\s+\S+){2}/, { message: "That does not look like a valid address" }),
+  facebook: Yup.string().matches(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+    , { message: "That is not a valid URL" }),
+  twitter: Yup.string().matches(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+    , { message: "That is not a valid URL" }),
+  instagram: Yup.string().matches(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+    , { message: "That is not a valid URL" })
+});
 
 const ListingForm = (props) => {
-  const toggleCheckbox = label => {
-    if (this.selectedCheckboxes.has(label)) {
-      this.selectedCheckboxes.delete(label);
-    } else {
-      this.selectedCheckboxes.add(label);
-    }
-  }
-
-  // const createCheckbox = option => (
-  //   <Checkbox
-  //     label={option}
-  //     isSelected={form.chosen[option]}
-  //     onCheckboxChange={toggleCheckbox}
-  //     key={option}
-  //   />
-  // );
-
+  const { user } = useAuth0();
   return (
-    <div class="container">
+    <div className="container">
       <h4 style={{ color: "black" }}>Create your business profile</h4>
       <Formik
         initialValues={{
-          user: props.user,
-          email: props.email, name: "", address1: "",
+          user: user,
+          email: user.email,
+          name: "",
+          address1: "",
           address2: "",
           city: "",
           us_state: "",
-          zip: "",
+          zip_code: "",
           image: "",
           telephone: "",
           website: "",
@@ -45,125 +55,104 @@ const ListingForm = (props) => {
           instagram: "",
           options: ["Dining", "Essentials", "Take-Out", "Delivery", "Retail", "Services"],
           chosen: []
-        }
-        }
-        validate={values => {
-          const errors = {};
-          if (/^\d+$/.test(values.email) === false) {
-            errors.zip = "That is not a valid Zip code"
-          }
-          return errors
         }}
+        validationSchema={ListingSchema}
         onSubmit={(values, { setSubmitting }) => {
           console.log(values)
         }}
       >
         {({
-          values,
           errors,
           touched,
+          values,
           handleChange,
           handleSubmit,
         }) => (
             <Form onSubmit={handleSubmit}>
-              <Input
+              <Field
                 name="name"
                 type="text"
                 placeholder="Business Name (required)"
-                value={values.name}
                 className="col-11 signup-boxes"
-                onChange={handleChange}
               />
-              <Input
+              <Field
                 name="address1"
                 type="text"
-                value={values.address1}
                 className="col-11 signup-boxes"
                 placeholder="Address (required)"
-                onChange={handleChange}
               />
-              <Input
+              {errors.address1 ? <p>{errors.address1}</p> : null}
+              <Field
                 name="address2"
                 type="text"
-                value={values.address2}
                 className="col-11 signup-boxes"
                 placeholder="Address"
-                onChange={handleChange}
               />
-              <Input
+              <Field
                 name="city"
                 type="text"
-                value={values.city}
                 className="col-5 signup-boxes"
                 placeholder="City"
-                onChange={handleChange}
               />
-              <Input
+              <Field
                 name="us_state"
                 type="text"
-                value={values.us_state}
                 className="col-2 signup-boxes"
                 placeholder="State"
-                onChange={handleChange}
               />
-              <Input
-                name="zip"
+              <Field
+                name="zip_code"
                 type="text"
-                value={values.zip}
                 className="col-4 signup-boxes"
-                placeholder="Zip*"
-                onChange={handleChange}
+                placeholder="Zip"
               />
-              <p>{errors.zip && touched.zip && errors.zip}</p>
-              <Input
+              {errors.zip_code ? <p>{errors.zip_code}</p> : null}
+              <Field
                 name="telephone"
                 type="text"
-                value={values.telephone}
                 className="col-11 signup-boxes"
                 placeholder="Telephone*"
                 onChange={handleChange}
               />
-              <Input
+              <Field
                 name="image"
                 type="text"
                 className="col-11 signup-boxes"
-                value={values.user ? values.user.user.picture : ""}
                 placeholder="Photo (paste in url)"
-                onChange={handleChange}
               />
-              <Input
+              <Field
                 name="website"
                 type="text"
-                value={values.website}
                 className="col-11 signup-boxes"
                 placeholder="Website"
-                onChange={handleChange}
               />
-              <Input
+              <Field
                 name="twitter"
                 type="text"
-                value={values.twitter}
                 className="col-4 signup-boxes"
                 placeholder="Twitter (full URL)"
-                onChange={handleChange}
               />
-              <Input
+              <Field
                 name="facebook"
                 type="text"
-                value={values.facebook}
                 className="col-3 signup-boxes"
                 placeholder="Facebook (full URL)"
-                onChange={handleChange}
               />
-              <Input
+              <Field
                 name="instagram"
                 type="text"
-                value={values.instagram}
                 className="col-4 signup-boxes"
                 placeholder="Instagram (full URL)"
-                onChange={handleChange}
               />
               <h4 style={{ color: "black" }}>Categories (check all that apply)</h4>
+              {values.options.map((item, index) => (
+                <>
+                  <label className="form-check-label">
+                    <Field type="checkbox" name="chosen" value={item} />
+                    {item}
+                  </label>
+                </>
+              ))}
               <div className="col-12">
                 <h4 style={{ color: "black" }}>Additional details</h4>
                 <textarea className="form-control" rows="10" name="description" onChange={handleChange} placeholder="Message to your customers, store hours, etc." />
@@ -183,7 +172,7 @@ const ListingSignup = props => {
   if (!isAuthenticated) {
     return <Redirect to="login" />
   } else {
-    return <ListingForm {...user} />
+    return <ListingForm />
   }
 }
 
