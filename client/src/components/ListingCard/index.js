78e27a82be88ae6ddd3d089444f6351e8043ff84
JustinRice <jsrice7391@gmail.com>
2020-04-30 21:05:04 -0400

    Some tweaks

--- client/src/components/ListingCard/index.js
+++ client/src/components/ListingCard/index.js
@@ -4,6 +4,7 @@ import Twitter from "../../images/twitter-logo.png";
 import Instagram from "../../images/instagram-logo.png";
 import Iframe from 'react-iframe'
 import "./listingcard.css"
+import { Map } from "../Map/index";
 // import useScript from '../useScript';
 
 function ListingCard(props) {
@@ -12,31 +13,25 @@ function ListingCard(props) {
   return (
     <div>
       <div className="results-box">
-          <div className="col-2 image text">
+        <div className="col-2 image text">
           <img src={props.image} alt="..." className="img-thumbnail"></img>
           <div className="text">{props.address1}</div>
-              {props.address2 ? (<div className="address">{props.address2}</div>) : (<div></div>)}
-              <div className="text">{props.city}, {props.us_state} {props.zip_code}</div>
-              <div className="text">{props.telephone}</div> 
-              <div className="text"><a href={props.website} target="_blank" rel="noopener noreferrer">{props.website}</a></div> 
-          </div>
-          <div className="col-6 text">
-              <div className="listing-title">{props.name}</div>
-              <div className="description">{props.description}</div> 
-              {props.twitter ? (<div className="twitter" style={{display: "inline-block"}}><a href={props.twitter} target="_blank" rel="noopener noreferrer"><img src={Twitter} style={{width: "30px", margin: "5px" }} /></a></div>) : (<div></div>)}
-              {props.facebook ? (<div className="facebook" style={{display: "inline-block"}} ><a href={props.facebook} target="_blank" rel="noopener noreferrer"><img src={Facebook} style={{width: "30px", margin: "5px" }} /></a></div>) : (<div></div>)}
-              {props.instagram ? (<div className="instagram" style={{display: "inline-block"}}><a href={props.instagram} target="_blank" rel="noopener noreferrer"><img src={Instagram} style={{width: "30px", margin: "5px" }} /></a></div>) : (<div></div>)}
-              </div>
-              <div className="col-3 image">
-                {console.log("ADDRESS ", props.address1)}
-              <Iframe url={"https://www.google.com/maps/embed/v1/place?key=AIzaSyAL4yfd8ajam3YQXWrvocecV0PmURldsxs&zoom=11&q="+props.address1.replace(/ /g, "+")+"%20"+props.city.replace(/ /g, "+")+"%2C%20"+props.us_state.replace(/ /g, "+")+"%20"+props.zip_code}
-                width="300px"
-                height="150px"
-                id="myId"
-                className="map"
-                display="initial"
-                position="relative"/>
-              </div>
+          {props.address2 ? (<div className="address">{props.address2}</div>) : (<div></div>)}
+          <div className="text">{props.city}, {props.us_state} {props.zip_code} </div>
+          <p>{props.telephone}</p>
+          <div className="text"><a href={props.website} target="_blank" rel="noopener noreferrer">{props.website}</a></div>
+        </div>
+        <div className="col-6 text">
+          <div className="listing-title">{props.name}</div>
+          <div className="description">{props.description}</div>
+          {props.twitter ? (<div className="twitter" style={{ display: "inline-block" }}><a href={props.twitter} target="_blank" rel="noopener noreferrer"><img src={Twitter} style={{ width: "30px", margin: "5px" }} /></a></div>) : (<div></div>)}
+          {props.facebook ? (<div className="facebook" style={{ display: "inline-block" }} ><a href={props.facebook} target="_blank" rel="noopener noreferrer"><img src={Facebook} style={{ width: "30px", margin: "5px" }} /></a></div>) : (<div></div>)}
+          {props.instagram ? (<div className="instagram" style={{ display: "inline-block" }}><a href={props.instagram} target="_blank" rel="noopener noreferrer"><img src={Instagram} style={{ width: "30px", margin: "5px" }} /></a></div>) : (<div></div>)}
+        </div>
+        <div className="col-3 image">
+          {console.log("ADDRESS ", props.address1)}
+          <Map {...props} />
+        </div>
       </div>
       <hr />
     </div>
---
+++ client/src/components/Map/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Iframe from 'react-iframe'
+
+export const Map = (props) => (
+    <Iframe url={"https://www.google.com/maps/embed/v1/place?key=AIzaSyAL4yfd8ajam3YQXWrvocecV0PmURldsxs&zoom=11&q=" + props.address1.replace(/ /g, "+") + "%20" + props.city.replace(/ /g, "+") + "%2C%20" + props.us_state.replace(/ /g, "+") + "%20" + props.zip_code}
+        width="300px"
+        height="150px"
+        id="myId"
+        className="map"
+        display="initial"
+        position="relative" />
+)
