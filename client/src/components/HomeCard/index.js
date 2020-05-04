973a120d0fb32c6a6141eb226cf79e6408ecebcd
David Hauck <dhauck@mac.com>
2020-05-03 22:46:30 -0400

    changed to more universal design, cleaned up old stylesheets

--- client/src/components/HomeCard/index.js
+++ client/src/components/HomeCard/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import SearchForm from "../../components/SearchForm"
-import TownHall from "../../images/hamilton-background3.jpg"
+import TownHall from "../../images/farmers-market-background.jpg"
 import API from "../../utils/API";
 import { FeatureCard } from "../Decorators"
 import "./homecard.css";
@@ -17,17 +17,20 @@ const HomeCard = () => {
 
   return (
     <div>
-      <div className="fill"><img alt="..." src={TownHall} /></div>
+      {/* <div className="fill"><img alt="..." src={TownHall} /></div> */}
       <div className="display-text">
         <h5 className="search-text">Support your local businesses</h5>
         <SearchForm/>
-        <div className="search-text">Own a business? <a href="/new-listing/">Add your listing</a></div>
+        {/* <div className="below-image">Own a business? <a href="/new-listing/">Add your listing</a></div> */}
       </div>
       <div className="below-image">
-        <h1 className="lower-title">Virtual Hamilton-Wenham</h1>
-        <p className="lower-subhead">Virtual Hamilton-Wenham is a non-profit branch of Kinship Farm dedicated to supporting the ability of local businesses to survive and thrive. This is a centralized portal, not unlike our beautiful downtown, allowing people to browse, shop, and enjoy what Hamilton-Wenham has to offer!</p>
-        <h1>Featured Business:</h1>
+        {/* <h1 className="lower-title">Virtual Hamilton-Wenham</h1> */}
+        {/* <p className="lower-subhead">Virtual Hamilton-Wenham is a non-profit branch of Kinship Farm dedicated to supporting the ability of local businesses to survive and thrive. This is a centralized portal, not unlike our beautiful downtown, allowing people to browse, shop, and enjoy what Hamilton-Wenham has to offer!</p> */}
+        {/* <h1>Featured Business:</h1> */}
         <FeatureCard {...featured.featured} />
+        <FeatureCard {...featured.featured} />
+        <FeatureCard {...featured.featured} />
+
       </div>
     </div>
   );
