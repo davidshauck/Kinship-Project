be802cecac790e495b30f8511ff81ae66b625344
jsrice7391 <jsrice7391@gmail.com>
2020-05-03 20:27:21 -0400

    feat(upload-works): Uploading works from the API with form data, going to work on adding it to react.

---
+++ routes/api/upload.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const multer = require("multer");
+const AWS = require("aws-sdk");
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage
+});
+
+
+
+router.route("/new").post(upload.single("file"), (req, res) => {
+    const file = req.file;
+    const s3FileURL = process.env.AWS_Uploaded_File_URL_LINK;
+
+    let s3bucket = new AWS.S3({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION
+    });
+
+    //Where you want to store your file
+
+    var params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: file.originalname,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+        ACL: "public-read"
+    };
+
+    s3bucket.upload(params, function (err, data) {
+        if (err) {
+            res.status(500).json({
+                error: true,
+                Message: err
+            });
+        } else {
+
+            var newFileUploaded = {
+                description: req.body.description,
+                fileLink: s3FileURL + file.originalname,
+                s3_key: params.Key
+            };
+
+            res.json({
+                ...newFileUploaded
+            })
+        }
+    });
+})
+
+
+module.exports = router;
