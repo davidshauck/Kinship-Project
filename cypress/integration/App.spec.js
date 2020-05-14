a6dd6448a594251ede588f3656b58481bd68d7cc
JustinRice <jsrice7391@gmail.com>
2020-05-14 19:56:44 -0400

    Added cypress tests

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -4,3 +4,11 @@ describe ('First Test', () => {
     });
   });
   
+
+  describe('My First Test', () => {
+    it('clicks the link "type"', () => {
+      cy.visit("http://localhost:3000/")
+
+      cy.get(".navbar-nav").find('li')
+    })
+  })
